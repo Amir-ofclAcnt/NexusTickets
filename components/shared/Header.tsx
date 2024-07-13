a7ec5ec82986402420837353e451efecdf557b99
Amir <hosseiniamir747@gmail.com>
2024-07-13 12:10:13 +0200

    created part of the database

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,11 +1,4 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  RedirectToSignIn,
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
