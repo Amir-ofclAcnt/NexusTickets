3df2e2248fe1d5a96ef6189753ae14263f2ab6d9
Amir <hosseiniamir747@gmail.com>
2024-07-16 16:18:37 +0200

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
-import { ClerkProvider, RedirectToSignIn, RedirectToSignUp } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
