d9f2608c84a395a5ffebf30ffb3424602072ea38
Amir <hosseiniamir747@gmail.com>
2024-07-18 18:15:16 +0200

    -

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,25 +1,18 @@
+require("dotenv").config();
+const uri = process.env.MONGODB_URI;
+
 const { MongoClient } = require("mongodb");
 
 async function connectToDatabase() {
-  // Replace the uri string with your MongoDB deployment's connection string.
-  const uri =
-    "mongodb+srv://AmirDB:123123123@cluster0.a1yptao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   try {
-    // Connect to the MongoDB cluster
     await client.connect();
-
-    // Make the appropriate DB calls
-    const database = client.db("database_name"); // Replace with your database name
     console.log("Connected to database");
-
-    // Example: List collections
-    const collections = await database.listCollections().toArray();
-    console.log("Collections:", collections);
+    // Perform database operations here
   } catch (err) {
     console.error(err);
   } finally {
