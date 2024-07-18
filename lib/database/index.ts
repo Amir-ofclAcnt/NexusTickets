db725538a39051f801eb1b6e75e99a97fe973dcb
Amir <hosseiniamir747@gmail.com>
2024-07-18 18:46:23 +0200

    -

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,18 +1,26 @@
-require("dotenv").config();
-const uri = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const { MongoClient } = require("mongodb");
 
 async function connectToDatabase() {
+  // Replace the uri string with your MongoDB deployment's connection string.
+  const uri = MONGODB_URI; // Replace with your actual connection string
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   try {
+    // Connect to the MongoDB cluster
     await client.connect();
+
+    // Make the appropriate DB calls
+    const database = client.db(" NEXUSTICKETS"); // Replace with your database name
     console.log("Connected to database");
-    // Perform database operations here
+
+    // Example: List collections
+    const collections = await database.listCollections().toArray();
+    console.log("Collections:", collections);
   } catch (err) {
     console.error(err);
   } finally {
