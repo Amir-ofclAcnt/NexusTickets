e081c81b872b48650b50f34286680ee6b5daeebb
Amir <hosseiniamir747@gmail.com>
2024-07-29 14:50:41 +0200

    -

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -231,8 +231,10 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                       Start Date:
                     </p>
                     <DatePicker
-                      selected={field.value}
-                      onChange={(date: Date) => field.onChange(date)}
+                      selected={field.value as Date} // Explicitly cast to Date
+                      onChange={(date: Date | null) => {
+                        field.onChange(date?.toISOString() || "");
+                      }}
                       showTimeSelect
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
@@ -264,7 +266,9 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                     </p>
                     <DatePicker
                       selected={field.value}
-                      onChange={(date: Date) => field.onChange(date)}
+                      onChange={(date: Date | null) => {
+                        field.onChange(date?.toISOString() || "");
+                      }}
                       showTimeSelect
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
@@ -299,30 +303,30 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                       {...field}
                       className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
                     />
-                    <FormField
-                      control={form.control}
-                      name="isFree"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormControl>
-                            <div className="flex items-center">
-                              <label
-                                htmlFor="isFree"
-                                className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                              >
-                                Free Ticket
-                              </label>
-                              <Checkbox
-                                onCheckedChange={field.onChange}
-                                checked={field.value}
-                                id="isFree"
-                                className="mr-2 h-5 w-5 border-2 border-primary-500"
-                              />
-                            </div>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isFree"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="isFree"
+                      className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      Free Ticket
+                    </label>
+                    <Checkbox
+                      onCheckedChange={(checked) => field.onChange(checked)}
+                      checked={field.value}
+                      id="isFree"
+                      className="mr-2 h-5 w-5 border-2 border-primary-500"
                     />
                   </div>
                 </FormControl>
@@ -330,6 +334,9 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
               </FormItem>
             )}
           />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
           <FormField
             control={form.control}
             name="url"
