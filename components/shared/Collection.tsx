34821de321d8268572fa38203ccf998f15e0de23
Amir <hosseiniamir747@gmail.com>
2024-08-03 18:48:19 +0200

    -

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -31,7 +31,7 @@ const Collection = ({
          flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center"
         >
           <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
-          <p className="">{emptyStateSubtext}</p>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
         </div>
       )}
     </>
