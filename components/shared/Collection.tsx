137c4443c52ee96827a131f0e49f17c12cfe6ff9
Amir <hosseiniamir747@gmail.com>
2024-08-04 22:13:40 +0200

    -

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -28,8 +28,17 @@ const Collection = ({
           <ul
             className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 
         lg:grid-cols-3 xl:gap-10"
-          ></ul>
-          {data[0].title}
+          >
+            {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organized";
+
+              const hidePrice = collectionType === "My_Tickets";
+
+              return (
+                <li key={event._id} className="flex justify-center"></li>
+              )
+            })}
+          </ul>
         </div>
       ) : (
         <div
