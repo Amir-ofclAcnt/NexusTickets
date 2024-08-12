51cba3b979124802505b12cc451ff79cf80b5df6
Amir <hosseiniamir747@gmail.com>
2024-08-12 20:51:08 +0200

    -

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -2,6 +2,7 @@ import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const hasEventFinished =  new Date(event.endDateTime) < new Date()
   return <div>CheckoutButton</div>;
 };
 
